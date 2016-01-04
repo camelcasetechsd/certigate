51f7f2329646e437e802c49287162a2a1e827a04
Mohamed Labib <lebaz20@gmail.com>
2016-01-04 17:25:19 +0200

    #1 - Add Status constants class

---
+++ module/Utilities/src/Utilities/Service/Status.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Utilities\Service;
+
+/**
+ * Status
+ * 
+ * Hold Status related constants
+ * 
+ * 
+ * @package utilities
+ * @subpackage service
+ */
+class Status {
+
+    
+    /**
+     * Entity is active
+     */
+    const STATUS_ACTIVE = 1;
+    
+    /**
+     * Entity is active text
+     */
+    const STATUS_ACTIVE_TEXT = "Active";
+    
+    /**
+     * Entity is inactive
+     */
+    const STATUS_INACTIVE = 0;
+    
+    /**
+     * Entity is inactive text
+     */
+    const STATUS_INACTIVE_TEXT = "InActive";
+    
+    /**
+     * Entity is deleted
+     */
+    const STATUS_DELETED = 2;
+    
+    /**
+     * Entity is deleted text
+     */
+    const STATUS_DELETED_TEXT = "Deleted";
+
+}
