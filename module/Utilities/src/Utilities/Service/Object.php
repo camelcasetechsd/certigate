26356e836e3f724922e22f1ff30e58cd499e93d7
Mohamed Labib <lebaz20@gmail.com>
2016-01-04 17:24:58 +0200

    #1 - Add Object utilities service

---
+++ module/Utilities/src/Utilities/Service/Object.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Utilities\Service;
+
+use Utilities\Service\Status;
+
+/**
+ * Object
+ * 
+ * Handles Object-related operations
+ * 
+ * 
+ * @package utilities
+ * @subpackage service
+ */
+class Object {
+
+    
+    /**
+     * prepare object for display
+     * 
+     * 
+     * @access public
+     * @param array $objectsArray
+     * @return array objects prepared for display
+     */
+    public function prepareForDisplay(array $objectsArray) {
+        foreach($objectsArray as $object){
+            $objectProperties = get_object_vars($object);
+            if (array_key_exists("status", $objectProperties)){
+                switch ($object->status) {
+                    case Status::STATUS_ACTIVE:
+                        $object->status = Status::STATUS_ACTIVE_TEXT;
+                        break;
+                    case Status::STATUS_INACTIVE:
+                        $object->status = Status::STATUS_INACTIVE_TEXT;
+                        break;
+                    case Status::STATUS_DELETED:
+                        $object->status = Status::STATUS_DELETED_TEXT;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            foreach ($objectProperties as $objectPropertyName => $objectPropertyValue){
+                if($objectPropertyValue instanceof \DateTime){
+                    $object->$objectPropertyName = $objectPropertyValue->format("D, d M Y H:i");
+                }
+            }
+        }
+        return $objectsArray;
+    }
+
+}
