bcd89fb079ff4851dc68c146365140d1f26c5869
Mohamed Labib <lebaz20@gmail.com>
2016-01-04 17:29:11 +0200

    #1 - Add object utilities service configuration

--- module/Utilities/config/module.config.php
+++ module/Utilities/config/module.config.php
@@ -4,9 +4,13 @@ return array(
     'service_manager' => array(
         'aliases' => array(
             'wrapperQuery' => 'Utilities\Service\Query',
+            'objectUtilities' => 'Utilities\Service\Object',
         ),
         'factories' => array(
             'Utilities\Service\Query' => 'Utilities\Service\Query\QueryFactory',
-        )
         ),
+        'invokables' => array(
+            'Utilities\Service\Object' => 'Utilities\Service\Object',
+        )
+    ),
 );
