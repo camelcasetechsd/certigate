178ca5fd4e5d6dedca8a13ede81aacd8da761a5f
Mohamed Labib <lebaz20@gmail.com>
2016-01-05 11:22:15 +0200

    #1 Page CRUD - Add WYSIWYG CK editor

---
+++ public/js/pageCRUD.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    // Replace the <textarea id="page_form_body"> with a CKEditor
+    // instance, using default configuration.
+    CKEDITOR.replace('page_form_body');
+
+});
+
