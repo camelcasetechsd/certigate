d6c79b3b4aebb96efea4fd3fdd45108aed2187fa
Mohamed Labib <lebaz20@gmail.com>
2016-01-06 15:49:28 +0200

    #2 Menu Crud - Add support for filtering by page existance

--- module/CMS/src/CMS/Entity/MenuItemRepository.php
+++ module/CMS/src/CMS/Entity/MenuItemRepository.php
@@ -21,39 +21,41 @@ class MenuItemRepository extends EntityRepository {
      * @param array $hiddenMenuItemsIds ,default is empty array
      * @param bool $menuItemStatus ,default is null
      * @param bool $menuStatus ,default is null
+     * @param bool $withPagesOnlyFlag ,default is false
      * @param bool $treeFlag ,default is false
      * @return array
      */
-    public function getMenuItemsSorted($hiddenMenuItemsIds = array(), $menuItemStatus = null, $menuStatus = null, $treeFlag = false) {
+    public function getMenuItemsSorted($hiddenMenuItemsIds = array(), $menuItemStatus = null, $menuStatus = null, $withPagesOnlyFlag = false, $treeFlag = false) {
         $repository = $this->getEntityManager();
         $queryBuilder = $repository->createQueryBuilder('mt');
         $parameters = array();
         $queryBuilder->select('mt')
                 ->from("CMS\Entity\MenuItem", "mt")
                 ->orderBy('mt.menu,mt.weight', 'ASC');
-        if(count($hiddenMenuItemsIds) > 0){
+        if (count($hiddenMenuItemsIds) > 0) {
             $parameters['hiddenMenuItemsIds'] = $hiddenMenuItemsIds;
             $queryBuilder->andWhere($queryBuilder->expr()->eq('mt.id', ":hiddenMenuItemsIds"));
         }
-        if( !is_null($menuItemStatus) ){
+        if (!is_null($menuItemStatus)) {
             $parameters['menuItemStatus'] = $menuItemStatus;
             $queryBuilder->andWhere($queryBuilder->expr()->eq('mt.status', ":menuItemStatus"));
-        } 
-        if( !is_null($menuStatus) ){
+        }
+        if (!is_null($menuStatus)) {
             $parameters['menuStatus'] = $menuStatus;
             $queryBuilder->innerJoin('CMS\Entity\Menu', 'm', Join::WITH, $queryBuilder->expr()->eq('mt.menu', 'm.id'))
                     ->andWhere($queryBuilder->expr()->eq('m.status', ":menuStatus"));
-        } 
-        if(count($parameters) > 0){
+        }
+        if ($withPagesOnlyFlag === true) {
+            $queryBuilder->innerJoin('mt.page', 'p');
+        }
+        if (count($parameters) > 0) {
             $queryBuilder->setParameters($parameters);
         }
         $menuItems = $queryBuilder->getQuery()->getResult();
 
         $menuItemModel = new MenuItemModel();
-        $menuItemsTree = $menuItemModel->getSortedMenuItems($menuItems, /*$root =*/ 0, $treeFlag);
+        $menuItemsTree = $menuItemModel->getSortedMenuItems($menuItems, /* $root = */ 0, $treeFlag);
         return $menuItemsTree;
     }
-
-    
 
 }
