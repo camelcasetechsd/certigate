0acea439dc107ec2b831dfb3e3ea2195114cf274
Adham Saad <adham.sa3d@gmail.com>
2016-01-07 13:25:04 +0200

    open cart installation

--- chef/site-cookbooks/certigate/recipes/default.rb
+++ chef/site-cookbooks/certigate/recipes/default.rb
@@ -1,23 +1,26 @@
 include_recipe "certigate::php_extensions"
 
+# install mysql chef gem
 mysql2_chef_gem 'default' do
     client_version node['mysql']['version'] if node['mysql'] && node['mysql']['version']
     action :install
 end
 
-mysql_service "#{node.db.name}" do
-    port '3306'
+# install mysql server
+mysql_service "default" do
+    port "#{node.mysql.root_connection.port}"
     version '5.5'
     initial_root_password "#{node.mysql.root_connection.password}"
     socket "/var/run/mysqld/mysqld.sock"
     action [:create, :start]
 end
 
-mysql_database "certigate" do
+# create default database
+mysql_database "#{node.site.db.name}" do
     connection node['mysql']['root_connection']
-    owner "#{node.db.username}"
-    encoding  'utf8'
-    collation  'utf8_general_ci'
+    owner "#{node.site.db.username}"
+    encoding  "#{node.site.db.encoding}"
+    collation  "#{node.site.db.collation}"
     action :create
 end
 
@@ -38,15 +41,21 @@ apache_module "mpm_event" do
     enable false
 end
 
+# add record to hosts file
 hostsfile_entry '127.0.0.1' do
     hostname "#{node.site.host}"
     action :append
 end
 
+# install bower
 execute "bower" do
   command "sudo npm install -g bower"
 end
 
+# remove unneeded index.html
 execute "remove index.html" do
   command "sudo rm /var/www/html/index.html -f"
 end
+
+# open cart estore
+include_recipe "certigate::opencart_estore"
---
+++ chef/site-cookbooks/certigate/recipes/opencart_estore.rb
@@ -0,0 +1,22 @@
+# create open cart database
+mysql_database "#{node.estore.db.name}" do
+    connection node['mysql']['root_connection']
+    owner "#{node.estore.db.username}"
+    encoding  "#{node.estore.db.encoding}"
+    collation  "#{node.estore.db.collation}"
+    action :create
+end
+
+# Initialize web app
+web_app "certigate_estore" do
+    template "default.conf.erb"
+    server_name "#{node.estore.host}"
+    docroot "#{node.estore.public_path}"
+    environment "#{node.estore.environment}"
+end
+
+# add record to /etc/hosts file
+hostsfile_entry '127.0.0.1' do
+    hostname "#{node.estore.host}"
+    action :append
+end
