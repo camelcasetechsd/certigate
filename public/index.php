0d72e96556790774e5efa8b30ab6062b3c18b72e
Mohamed Labib <lebaz20@gmail.com>
2016-01-13 16:24:00 +0200

    #8 Users Captcha - Add application path global environment variable

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@
  * to the application root now.
  */
 chdir(dirname(__DIR__));
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', (getenv('APPLICATION_PATH') ? getenv('APPLICATION_PATH') : __DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
