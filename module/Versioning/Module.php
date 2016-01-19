eef4d22e60bb4511dc00b1f52ad1e31933765d7c
Mohamed Labib <lebaz20@gmail.com>
2016-01-19 11:39:46 +0200

    Versioning - Integrate versioning module configuration into App

--- module/Versioning/Module.php
+++ module/Versioning/Module.php
@@ -9,6 +9,17 @@ use Gedmo\Loggable\LoggableListener;
 class Module
 {
 
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace( '\\', '/', __NAMESPACE__ ),
+                ),
+            ),
+        );
+    }
+
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
