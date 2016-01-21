0857a4ce0ba6493b917111fa2fa45eeffe654ac6
Mohamed Labib <lebaz20@gmail.com>
2016-01-21 11:49:07 +0200

    #13 Course Resources Generic logger - Add generic logger in utilities module - Logs are written in logs/error.log - Logger is accessible via service locator as loggerUtilities

--- module/Utilities/config/module.config.php
+++ module/Utilities/config/module.config.php
@@ -6,11 +6,13 @@ return array(
             'wrapperQuery' => 'Utilities\Service\Query',
             'cacheUtilities' => 'Utilities\Service\Cache',
             'objectUtilities' => 'Utilities\Service\Object',
+            'loggerUtilities' => 'Utilities\Service\Logger',
         ),
         'factories' => array(
             'Utilities\Service\Query' => 'Utilities\Service\Query\QueryFactory',
             'Utilities\Service\Cache' => 'Utilities\Service\Cache\CacheFactory',
             'Utilities\Service\Object' => 'Utilities\Service\ObjectFactory',
+            'Utilities\Service\Logger' => 'Utilities\Service\Logger\LoggerFactory',
         )
     ),
 );
