085a53d3a9edc20c38d7317b5c6827a5347f543b
Mohamed Labib <lebaz20@gmail.com>
2016-01-21 11:55:09 +0200

    #13 Course Resources Delete physically delete resource file - Successful entity removal is upon resource file successful deletion - Error message should be logged

--- module/Courses/src/Courses/Model/Resource.php
+++ module/Courses/src/Courses/Model/Resource.php
@@ -11,6 +11,7 @@ use Utilities\Service\Status;
  * 
  * 
  * @property Utilities\Service\Query\Query $query
+ * @property Zend\Log\Logger $logger
  * 
  * @package courses
  * @subpackage model
@@ -25,14 +26,22 @@ class Resource
     protected $query;
 
     /**
+     *
+     * @var Zend\Log\Logger
+     */
+    protected $logger;
+
+    /**
      * Set needed properties
      * 
      * @access public
      * @param Utilities\Service\Query\Query $query
+     * @param Zend\Log\Logger $logger
      */
-    public function __construct($query)
+    public function __construct($query, $logger)
     {
         $this->query = $query;
+        $this->logger = $logger;
     }
 
     /**
@@ -61,6 +70,32 @@ class Resource
     }
 
     /**
+     * Remove resource
+     * Remove resource file as well
+     * 
+     * @access public
+     * @param Courses\Entity\Resource $resource
+     */
+    public function remove($resource)
+    {
+        $processResult = "true";
+        try {
+            set_error_handler(function ($errorSeverity, $errorMessage) {
+                throw new \Exception($errorMessage, $errorSeverity);
+            });
+            $file = $resource->getFile()["tmp_name"];
+            unlink($file);
+            restore_error_handler();
+            $this->query->remove($resource);
+        } catch (\Exception $e) {
+            $this->logger->err($e->getMessage());
+            $processResult = "false";
+        }
+
+        return $processResult;
+    }
+
+    /**
      * Prepare resources for display
      * 
      * @access public
@@ -72,7 +107,7 @@ class Resource
     {
         $preparedResources = array();
         foreach ($resources as $resource) {
-            if($resource->getStatus() != Status::STATUS_ACTIVE){
+            if ($resource->getStatus() != Status::STATUS_ACTIVE) {
                 continue;
             }
             $preparedResources[$resource->getType()]["files"][] = array(
