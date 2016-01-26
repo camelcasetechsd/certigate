361395a44abed71ba11a5207ddba1764e29a197e
Mohamed Labib <lebaz20@gmail.com>
2016-01-26 13:57:06 +0200

    #13 Resource CRUD file display - Add extension filtration in HTML file input - Add supported extensions listing display in file input

--- module/Courses/src/Courses/Form/ResourceForm.php
+++ module/Courses/src/Courses/Form/ResourceForm.php
@@ -119,9 +119,13 @@ class ResourceForm extends Form {
             'name' => 'file',
             'type' => 'Zend\Form\Element\File',
             'attributes' => array(
+                'accept' => 'application/vnd.openxmlformats-officedocument.presentationml.slideshow,application/vnd.openxmlformats-officedocument.presentationml.template,application/vnd.openxmlformats-officedocument.presentationml.presentation,application/vnd.ms-powerpoint,application/zip,application/octet-stream,application/pdf,pptx,potx,ppsx,thmx',
             ),
             'options' => array(
-                'label' => 'File',
+                'label' => '<p class="required">File</p> <p>Supported Extensions: zip,pdf,ppt,pptx</p>',
+                'label_options' => array(
+                    'disable_html_escape' => true,
+                )
             ),
         ));
         
