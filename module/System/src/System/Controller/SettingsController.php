5cee644a7335ddb458a05b5caf40abae1247f98e
Mohamed Labib <lebaz20@gmail.com>
2016-01-30 20:43:06 +0200

    #25 Settings business - Add setting form - Add setting controller

---
+++ module/System/src/System/Controller/SettingsController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace System\Controller;
+
+use Utilities\Controller\ActionController;
+use Zend\View\Model\ViewModel;
+use System\Form\SettingForm;
+use System\Entity\Setting;
+
+/**
+ * Settings Controller
+ * 
+ * Settings entries listing
+ * 
+ * 
+ * 
+ * @package system
+ * @subpackage controller
+ */
+class SettingsController extends ActionController
+{
+
+    /**
+     * List Settings
+     * 
+     * 
+     * @access public
+     * 
+     * @return ViewModel
+     */
+    public function indexAction()
+    {
+        $variables = array();
+        $query = $this->getServiceLocator()->get('wrapperQuery')->setEntity('System\Entity\Setting');
+        $objectUtilities = $this->getServiceLocator()->get('objectUtilities');
+        
+        $data = $query->findAll(/*$entityName =*/null);
+        $variables['settings'] = $objectUtilities->prepareForDisplay($data);
+        return new ViewModel($variables);
+    }
+
+    /**
+     * Create new Setting
+     * 
+     * 
+     * @access public
+     * @uses Setting
+     * @uses SettingForm
+     * 
+     * @return ViewModel
+     */
+    public function newAction()
+    {
+        $variables = array();
+        $query = $this->getServiceLocator()->get('wrapperQuery')->setEntity('System\Entity\Setting');
+        $setting = new Setting();
+
+        $form = new SettingForm();
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $data = $request->getPost()->toArray();
+            $form->setInputFilter($setting->getInputFilter($query));
+            $form->setData($data);
+            if ($form->isValid()) {
+                $query->save($setting, $data);
+                
+                $url = $this->getEvent()->getRouter()->assemble(array('action' => 'index'), array('name' => 'systemSettings'));
+                $this->redirect()->toUrl($url);
+            }
+        }
+
+        $variables['settingForm'] = $this->getFormView($form);
+        return new ViewModel($variables);
+    }
+
+    /**
+     * Edit Setting
+     * 
+     * 
+     * @access public
+     * @uses SettingForm
+     * 
+     * @return ViewModel
+     */
+    public function editAction()
+    {
+        $variables = array();
+        $id = $this->params('id');
+        $query = $this->getServiceLocator()->get('wrapperQuery');
+        $setting = $query->find('System\Entity\Setting', $id);
+
+        $form = new SettingForm();
+        $form->bind($setting);
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $data = $request->getPost()->toArray();
+            $form->setInputFilter($setting->getInputFilter($query));
+            $form->setData($data);
+            if ($form->isValid()) {
+                $query->save($setting);
+                
+                $url = $this->getEvent()->getRouter()->assemble(array('action' => 'index'), array('name' => 'systemSettings'));
+                $this->redirect()->toUrl($url);
+            }
+        }
+
+        $variables['settingForm'] = $this->getFormView($form);
+        return new ViewModel($variables);
+    }
+    
+    /**
+     * Delete Setting
+     *
+     * 
+     * @access public
+     */
+    public function deleteAction()
+    {
+        $id = $this->params('id');
+        $query = $this->getServiceLocator()->get('wrapperQuery');
+        $setting = $query->find('System\Entity\Setting', $id);
+       
+        $query->remove($setting);
+        
+        $url = $this->getEvent()->getRouter()->assemble(array('action' => 'index'), array('name' => 'systemSettings'));
+        $this->redirect()->toUrl($url);
+    }
+
+
+}
+
---
+++ module/System/src/System/Form/SettingForm.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace System\Form;
+
+use Utilities\Form\Form;
+
+
+/**
+ * Setting Form
+ * 
+ * Handles Setting form setup
+ * 
+ * 
+ * @package system
+ * @subpackage form
+ */
+class SettingForm extends Form {
+    
+    /**
+     * setup form
+     * 
+     * 
+     * @access public
+     * @param string $name ,default is null
+     * @param array $options ,default is null
+     */
+    public function __construct($name = null, $options = null) {
+        parent::__construct($name, $options);
+
+        $this->setAttribute('class', 'form form-horizontal');
+
+        $this->add(array(
+            'name' => 'name',
+            'type' => 'Zend\Form\Element\Text',
+            'attributes' => array(
+                'required' => 'required',
+                'class' => 'form-control',
+            ),
+            'options' => array(
+                'label' => 'Name',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'value',
+            'type' => 'Zend\Form\Element\Text',
+            'attributes' => array(
+                'required' => 'required',
+                'class' => 'form-control',
+            ),
+            'options' => array(
+                'label' => 'Value',
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'id',
+            'type' => 'Zend\Form\Element\Hidden',
+        ));
+
+        $this->add(array(
+            'name' => 'Create',
+            'type' => 'Zend\Form\Element\Submit',
+            'attributes' => array(
+                'class' => 'btn btn-success',
+                'value' => 'Create',
+            )
+        ));
+        
+        $this->add(array(
+            'name' => 'reset',
+            'type' => 'Zend\Form\Element',
+            'attributes' => array(
+                'class' => 'btn btn-danger resetButton',
+                'value' => 'Reset',
+                'type' => 'button',
+            )
+        ));
+    }
+
+}
