d90ec219e1a69b29804a3ba1943954c91a2247c5
Mohamed Labib <lebaz20@gmail.com>
2016-01-31 15:17:32 +0200

    #15 Support multiple environments in question to answer - Fix environment variables setup and sql commands

--- public/q2a/updateDB.php
+++ public/q2a/updateDB.php
@@ -1,4 +1,8 @@
 <?php
 
 require_once __DIR__.'/qa-config.php';
-shell_exec("mysql -u ".QA_MYSQL_USERNAME." -p".QA_MYSQL_PASSWORD." -D ".QA_MYSQL_DATABASE." < ".__DIR__."/qaDumpSql.sql");
+$passwordPart = "";
+if(!empty(QA_MYSQL_PASSWORD)){
+    $passwordPart = "-p".QA_MYSQL_PASSWORD;
+}
+shell_exec("mysql -u ".QA_MYSQL_USERNAME." $passwordPart -D ".QA_MYSQL_DATABASE." < ".__DIR__."/qaDumpSql.sql");
