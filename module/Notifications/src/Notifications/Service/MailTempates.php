1df7479a63071fa47254acccc455d64be64aa4e1
Mohamed Labib <lebaz20@gmail.com>
2016-02-04 12:39:18 +0200

    #65 Notification mail templates - Added new mail templates - Updated mail templates constants

--- module/Notifications/src/Notifications/Service/MailTempates.php
+++ module/Notifications/src/Notifications/Service/MailTempates.php
@@ -15,14 +15,39 @@ class MailTempates {
 
     
     /**
-     * Exam Book Template
+     * Exam Approval Request Template
      */
-    const EXAM_BOOK_TEMPLATE = "exam-book";
+    const EXAM_APPROVAL_REQUEST_TEMPLATE = "exam-approval-request";
     
     /**
-     * Exam Book Admin Template
+     * New Exam Notification Template
      */
-    const EXAM_BOOK_ADMIN_TEMPLATE = "exam-book-admin";
+    const NEW_EXAM_NOTIFICATION_TEMPLATE = "new-exam-notification";
+
+    /**
+     * New Organization Notification Template
+     */
+    const NEW_ORGANIZATION_NOTIFICATION_TEMPLATE = "new-organization-notification";
+    
+    /**
+     * New User Notification Template
+     */
+    const NEW_USER_NOTIFICATION_TEMPLATE = "new-user-notification";
+    
+    /**
+     * New Course Notification Template
+     */
+    const NEW_COURSE_NOTIFICATION_TEMPLATE = "new-course-notification";
+            
+    /**
+     * New Instructor Welcome Kit Template
+     */
+    const NEW_INSTRUCTOR_WELCOME_KIT_TEMPLATE = "new-instructor-welcome-kit";
+    
+    /**
+     * New Organization Welcome Kit Template
+     */
+    const NEW_ORGANIZATION_WELCOME_KIT_TEMPLATE = "new-organization-welcome-kit";
     
     /**
      * Contact us Template
