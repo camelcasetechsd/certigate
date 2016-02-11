9a44561c975c72ba11ce6358bab7e0f74c6cd575
Mohamed Labib <lebaz20@gmail.com>
2016-02-11 12:24:17 +0200

    #55 Prepare approved version for not approved entity

--- module/Versioning/src/Versioning/Model/Version.php
+++ module/Versioning/src/Versioning/Model/Version.php
@@ -2,6 +2,10 @@
 
 namespace Versioning\Model;
 
+use Gedmo\Tool\Wrapper\EntityWrapper;
+use Doctrine\Common\Collections\Criteria;
+use Utilities\Service\Status;
+
 /**
  * Version Model
  * 
@@ -13,7 +17,8 @@ namespace Versioning\Model;
  * @package versioning
  * @subpackage model
  */
-class Version {
+class Version
+{
 
     /**
      *
@@ -27,7 +32,8 @@ class Version {
      * @access public
      * @param Utilities\Service\Query\Query $query
      */
-    public function __construct($query) {
+    public function __construct($query)
+    {
         $this->query = $query;
     }
 
@@ -39,12 +45,13 @@ class Version {
      * 
      * @return array array of prepared logs
      */
-    public function prepareLogs($entity) {
+    public function prepareLogs($entity)
+    {
         $logs = $this->query->setEntity('Gedmo\Loggable\Entity\LogEntry')->entityRepository->getLogEntries($entity);
         $preparedLogs = array();
         $logsKeys = array_keys($logs);
         $firstLogKey = reset($logsKeys);
-        foreach($logs as $logKey => $log){
+        foreach ($logs as $logKey => $log) {
             $preparedLogs[] = array(
                 'id' => $log->getId(),
                 'action' => $log->getAction(),
@@ -54,12 +61,109 @@ class Version {
                 'version' => $log->getVersion(),
                 'data' => $log->getData(),
                 'username' => $log->getUsername(),
-                'isLast' => ($logKey == $firstLogKey)?true:false,
+                'isLast' => ($logKey == $firstLogKey) ? true : false,
             );
         }
-        
+
         return $preparedLogs;
     }
 
+    /**
+     * Get logs for entries
+     * 
+     * @access public
+     * @param array $entities
+     * 
+     * @return array array of $entities' logs
+     */
+    public function getLogEntriesPerEntities($entities)
+    {
+        // assuming array of entities belong to them class
+        $entity = reset($entities);
+        $objectIds = array();
+        $wrapped = new EntityWrapper($entity, $this->query->entityManager);
+        $objectClass = $wrapped->getMetadata()->name;
+        // collect entitites ids
+        array_shift($entities);
+        $objectIds[] = $wrapped->getIdentifier();
+        foreach ($entities as $entity) {
+            $wrapped = new EntityWrapper($entity, $this->query->entityManager);
+            $objectIds[] = $wrapped->getIdentifier();
+        }
+
+        $logRepository = $this->query->setEntity('Gedmo\Loggable\Entity\LogEntry')->entityRepository;
+        $queryBuilder = $logRepository->createQueryBuilder("log");
+        $parameters = compact('objectIds', 'objectClass');
+
+        $queryBuilder->select("log")
+                ->addOrderBy('log.version', Criteria::DESC)
+                ->andWhere($queryBuilder->expr()->eq('log.objectClass', ":objectClass"))
+                ->andWhere($queryBuilder->expr()->in('log.objectId', ":objectIds"))
+                ->setParameters($parameters);
+
+        $logs = $queryBuilder->getQuery()->getResult();
+        
+        $logsGroupedByObjectId = array();
+        foreach($logs as $log){
+            $logsGroupedByObjectId[$log->getObjectId()][] = $log;
+        }
+        return $logsGroupedByObjectId;
+    }
+    
+    /**
+     * Get approved versions for currently not approved entities
+     * 
+     * @access public
+     * @param array $logsGroupedByObjectId
+     * @param string $statusProperty ,default is status
+     * @param string $activeStatusValue ,default is active status
+     * @return array approved versions
+     */
+    public function getNotApprovedDataApprovedVersions($logsGroupedByObjectId, $statusProperty = "status", $activeStatusValue = Status::STATUS_ACTIVE){
+        $notApprovedDataApprovedVersions = array();
+        foreach($logsGroupedByObjectId as $objectId => $logsPerObjectId){
+            foreach($logsPerObjectId as $log){
+                $versionData = $log->getData();
+                if(isset($versionData[$statusProperty]) && $versionData[$statusProperty] == $activeStatusValue){
+                    $notApprovedDataApprovedVersions[$objectId] = $versionData;
+                    break;
+                }
+            }
+        }
+        return $notApprovedDataApprovedVersions;
+    }
+    
+    /**
+     * Get approved data for not approved versions
+     * 
+     * @access public
+     * @param array $notApprovedData
+     * @param array $notApprovedDataApprovedVersions
+     * @return array approved data versions for not approved ones
+     */
+    public function getApprovedDataForNotApprovedOnes(&$notApprovedData, $notApprovedDataApprovedVersions){
+        foreach($notApprovedData as $notApprovedEntityKey => $notApprovedEntity){
+            $objectId = $notApprovedEntity->getId();
+            if(!array_key_exists($objectId, $notApprovedDataApprovedVersions)){
+                unset($notApprovedData[$notApprovedEntityKey]);
+                continue;
+            }
+            $notApprovedEntity->exchangeArray($notApprovedDataApprovedVersions[$objectId]);
+        }
+        return $notApprovedData;
+    }
+    
+    /**
+     * Get approved data for not approved data wrapper
+     * 
+     * @access public
+     * @param array $notApprovedData
+     * @return array approved data versions for not approved ones
+     */
+    public function getApprovedDataForNotApprovedOnesWrapper(&$notApprovedData){
+        $logsGroupedByObjectId = $this->getLogEntriesPerEntities($notApprovedData);
+        $notApprovedDataApprovedVersions = $this->getNotApprovedDataApprovedVersions($logsGroupedByObjectId);
+        $this->getApprovedDataForNotApprovedOnes($notApprovedData,$notApprovedDataApprovedVersions);
+    }
 
 }
