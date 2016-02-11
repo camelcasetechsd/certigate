426274042250f6b869f1cb69112684aed8ebc1f4
Mohamed Labib <lebaz20@gmail.com>
2016-02-11 14:12:10 +0200

    #55 Set outlines status not approved when edited by non-admin

--- module/Courses/Module.php
+++ module/Courses/Module.php
@@ -3,6 +3,10 @@
 namespace Courses;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\Mvc\MvcEvent;
+use Courses\Listener\CourseManagementListener;
+use Zend\Authentication\AuthenticationService;
+use Users\Entity\Role;
 
 class Module implements AutoloaderProviderInterface
 {
@@ -23,4 +27,26 @@ class Module implements AutoloaderProviderInterface
         return include __DIR__ . '/config/module.config.php';
     }
 
+    /**
+     * Add course management listener
+     * 
+     * @access public
+     * @param MvcEvent $event
+     */
+    public function onBootstrap( MvcEvent $event )
+    {
+        /* @var $eventManager \Doctrine\Common\EventManager */
+        $eventManager = $event->getApplication()->getServiceManager()->get( 'entitymanager' )->getEventManager();
+        $courseManagementListener = new CourseManagementListener;
+        $auth = new AuthenticationService();
+        $storage = $auth->getIdentity();
+        $isAdminUser = false;
+        if ($auth->hasIdentity()) {
+            if (in_array(Role::ADMIN_ROLE, $storage['roles'])) {
+                $isAdminUser = true;
+            }
+        }
+        $courseManagementListener->setIsAdminUser( $isAdminUser );
+        $eventManager->addEventSubscriber( $courseManagementListener );
+    }
 }
---
+++ module/Courses/src/Courses/Listener/CourseManagementListener.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Courses\Listener;
+
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Gedmo\Mapping\MappedEventSubscriber;
+use Utilities\Service\Status;
+use Courses\Entity\Outline;
+
+/**
+ * CourseManagement listener
+ * 
+ * Handles Related-to-course Entities' changes related business
+ *
+ * @property bool $isAdminUser
+ * 
+ * @package courses
+ * @subpackage listener
+ */
+class CourseManagementListener extends MappedEventSubscriber
+{
+
+    /**
+     *
+     * @var bool 
+     */
+    public $isAdminUser;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSubscribedEvents()
+    {
+        return array(
+            'onFlush',
+        );
+    }
+
+    /**
+     * Set isAdminUser
+     * 
+     * @access public
+     * @param bool $isAdminUser
+     */
+    public function setIsAdminUser($isAdminUser)
+    {
+        $this->isAdminUser = $isAdminUser;
+    }
+
+    /**
+     * Update status for course related entities
+     *
+     * @param OnFlushEventArgs $eventArgs
+     *
+     * @return void
+     */
+    public function onFlush(OnFlushEventArgs $eventArgs)
+    {
+        $entityManager = $eventArgs->getEntityManager();
+        $unitOfWork = $entityManager->getUnitOfWork();
+
+        if ($this->isAdminUser === false) {
+            foreach ($unitOfWork->getScheduledEntityUpdates() as $entity) {
+                if ($entity instanceof Outline) {
+                    $entity->setStatus(Status::STATUS_NOT_APPROVED);
+                    $classMetadata = $entityManager->getClassMetadata(get_class($entity));
+                    $unitOfWork->recomputeSingleEntityChangeSet($classMetadata, $entity);
+                }
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getNamespace()
+    {
+        return __NAMESPACE__;
+    }
+
+}
