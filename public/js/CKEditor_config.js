79216bbeafd11e4314fe2d35ceed39fc80a5b948
ahmedReda1 <se_ahmedreda@yahoo.com>
2016-02-17 15:01:52 +0200

    #73 fixing ckeditor not difined in pages without ckeditor

--- public/js/CKEditor_config.js
+++ public/js/CKEditor_config.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
+    if (typeof (CKEDITOR) !== "undefined") {
+        CKEDITOR.replace('page_form_body', {
+            filebrowserBrowseUrl: '/cms/page/browse',
+            filebrowserUploadUrl: '/cms/page/upload'
 
-    CKEDITOR.replace('page_form_body', {
-        filebrowserBrowseUrl: '/cms/page/browse',
-        filebrowserUploadUrl: '/cms/page/upload'
-
-    });
+        });
+    }
 });
 
