f2e92ccbf4a57a9edb44b9b4b20323dace0bb7c9
ahmedReda1 <se_ahmedreda@yahoo.com>
2016-02-24 13:02:20 +0200

    adding new global vars & renaming translation files & creating service for translation

--- module/CustomMustache/src/CustomMustache/Service/RendererFactory.php
+++ module/CustomMustache/src/CustomMustache/Service/RendererFactory.php
@@ -35,10 +35,11 @@ class RendererFactory implements FactoryInterface
      * @param ServiceLocatorInterface $serviceLocator
      * @return Renderer
      */
-    public function createService( ServiceLocatorInterface $serviceLocator )
+    public function createService(ServiceLocatorInterface $serviceLocator)
     {
 
-        $config = $serviceLocator->get( 'Configuration' );
+        $config = $serviceLocator->get('Configuration');
+        $translator = $serviceLocator->get('translatorHandler');
         $config = $config['mustache'];
 
         // set isProduction according to current environment
@@ -49,46 +50,60 @@ class RendererFactory implements FactoryInterface
 
         $config['helpers']['primaryMenu'] = '';
 
+//        $config['helpers']["i18n"] = function($text, &$translator) {
+//            return $translator->translate($text);
+//        };
+
+        $config['helpers']["i18n"] = function($text) {
+            /**
+             * TODO: Need to inject $translator object inside lambda_helper
+             * to be able to use translate function
+             */
+            return "TEST" . $text . "TEST";
+        };
+
         $forceFlush = !$config['helpers']['isProduction'];
-        $cmsCacheHandler = $serviceLocator->get( 'cmsCacheHandler' );
-        $menuView = $serviceLocator->get( 'cmsMenuView' );
+        $cmsCacheHandler = $serviceLocator->get('cmsCacheHandler');
+        $menuView = $serviceLocator->get('cmsMenuView');
 
         // TODO: Implement a better way to do this, allowing menu item hierarchy to be respected
         $path = $serviceLocator->get('request')->getUri()->getPath();
         $menuView->setActivePath($path);
 
-        $menusArray = $cmsCacheHandler->getCachedCMSData( $forceFlush );
-        $menusViewArray = $menuView->prepareMenuView( $menusArray[CacheHandler::MENUS_KEY], /* $menuTitleUnderscored = */ Menu::PRIMARY_MENU_UNDERSCORED );
-        $config['helpers']['primaryMenu'] = isset( $menusViewArray[Menu::PRIMARY_MENU_UNDERSCORED] ) ? $menusViewArray[Menu::PRIMARY_MENU_UNDERSCORED] : '';
+        $menusArray = $cmsCacheHandler->getCachedCMSData($forceFlush);
+        $menusViewArray = $menuView->prepareMenuView($menusArray[CacheHandler::MENUS_KEY], /* $menuTitleUnderscored = */ Menu::PRIMARY_MENU_UNDERSCORED);
+        $config['helpers']['primaryMenu'] = isset($menusViewArray[Menu::PRIMARY_MENU_UNDERSCORED]) ? $menusViewArray[Menu::PRIMARY_MENU_UNDERSCORED] : '';
 
 
         if ($auth->hasIdentity()) {
             $roles = $storage['roles'];
             $config['helpers']['loggedInUsername'] = $storage['username'];
         }
-        if (isset( $roles ) && in_array( Role::ADMIN_ROLE, $roles )) {
-            $adminMenu = $menuView->prepareMenuView( $menusArray[CacheHandler::MENUS_KEY],  Menu::ADMIN_MENU_UNDERSCORED , Menu::ADMIN_MENU_UNDERSCORED , Menu::ADMIN_MENU_UNDERSCORED);
-            $config['helpers']['adminMenu'] = isset( $adminMenu[Menu::ADMIN_MENU_UNDERSCORED] ) ? $adminMenu[Menu::ADMIN_MENU_UNDERSCORED] : '';
-        }   
-        
+        if (isset($roles) && in_array(Role::ADMIN_ROLE, $roles)) {
+            $adminMenu = $menuView->prepareMenuView($menusArray[CacheHandler::MENUS_KEY], Menu::ADMIN_MENU_UNDERSCORED, Menu::ADMIN_MENU_UNDERSCORED, Menu::ADMIN_MENU_UNDERSCORED);
+            $config['helpers']['adminMenu'] = isset($adminMenu[Menu::ADMIN_MENU_UNDERSCORED]) ? $adminMenu[Menu::ADMIN_MENU_UNDERSCORED] : '';
+        }
+
         /** @var $pathResolver \Zend\View\Resolver\TemplatePathStack */
-        $pathResolver = clone $serviceLocator->get( 'ViewTemplatePathStack' );
-        $pathResolver->setDefaultSuffix( $config['suffix'] );
+        $pathResolver = clone $serviceLocator->get('ViewTemplatePathStack');
+        $pathResolver->setDefaultSuffix($config['suffix']);
 
         /** @var $resolver \Zend\View\Resolver\AggregateResolver */
-        $resolver = $serviceLocator->get( 'ViewResolver' );
-        $resolver->attach( $pathResolver, 2 );
+        $resolver = $serviceLocator->get('ViewResolver');
+        $resolver->attach($pathResolver, 2);
 
-        $engine = new \Mustache_Engine( $this->setConfigs( $config ) );
+        $engine = new \Mustache_Engine($this->setConfigs($config, $serviceLocator));
 
         $renderer = new Renderer();
-        $renderer->setEngine( $engine );
-        $renderer->setSuffix( isset( $config['suffix'] ) ? $config['suffix'] : 'mustache'  );
-        $renderer->setSuffixLocked( (bool) $config['suffixLocked'] );
-        $renderer->setResolver( $resolver );
+        $renderer->setEngine($engine);
+        $renderer->setSuffix(isset($config['suffix']) ? $config['suffix'] : 'mustache' );
+        $renderer->setSuffixLocked((bool) $config['suffixLocked']);
+        $renderer->setResolver($resolver);
 
         return $renderer;
     }
+
+//    private 
 
     /**
      * Prepare config array
@@ -100,19 +115,21 @@ class RendererFactory implements FactoryInterface
      * @param array $config
      * @return array configuration array for mustache
      */
-    private function setConfigs( array $config )
+    private function setConfigs(array $config, $translator)
     {
         $options = array("extension" => ".phtml");
-        if (isset( $config["partials_loader"] )) {
+//        var_dump($config['partials_loader']);
+//        exit;
+        if (isset($config["partials_loader"])) {
             $path = $config["partials_loader"];
-            if (is_array( $config["partials_loader"] )) {
+            if (is_array($config["partials_loader"])) {
                 $path = $config["partials_loader"][0];
             }
-            $config["partials_loader"] = new \Mustache_Loader_FilesystemLoader( $path, $options );
+            $config["partials_loader"] = new \Mustache_Loader_FilesystemLoader($path, $options);
         }
 
-        if (isset( $config["loader"] )) {
-            $config["loader"] = new \Mustache_Loader_FilesystemLoader( $config["loader"][0], $options );
+        if (isset($config["loader"])) {
+            $config["loader"] = new \Mustache_Loader_FilesystemLoader($config["loader"][0], $options);
         }
         return $config;
     }
--- module/Translation/config/module.config.php
+++ module/Translation/config/module.config.php
@@ -3,20 +3,25 @@
 namespace Translation;
 
 return array(
-  'router' => array(),
-  'service_manager' => array(),
- 
-  'translator' => array(
-    'translation_file_patterns' => array(
-      array(
-        'type'     => 'gettext',
-        'base_dir' => __DIR__ . '/../language',
-        'pattern'  => '%s.mo',
-        'text_domain' => __NAMESPACE__,
-      ),
+    'router' => array(),
+    'service_manager' => array(
+        'factories' => array(
+            'CMS\Service\TranslatorHandler' => 'Translation\Service\Translator\TranslatorHandlerFactory',
+        ),
+        'aliases' => array(
+            'translatorHandler' => 'CMS\Service\TranslatorHandler',
+        ),
     ),
-  ),
- 
-  'controllers' => array(),
-  'view_manager' => array(),
+    'translator' => array(
+        'translation_file_patterns' => array(
+            array(
+                'type' => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern' => '%s.mo',
+                'text_domain' => __NAMESPACE__,
+            ),
+        ),
+    ),
+    'controllers' => array(),
+    'view_manager' => array(),
 );
