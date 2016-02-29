818ee6e8561ad3bd9db0eed841b9d6b989a71f2d
Mohamed Labib <lebaz20@gmail.com>
2016-02-29 19:08:21 +0200

    Unit test first working test case testing login action

--- module/DefaultModule/src/DefaultModule/Test/Controller/SignControllerTest.php
+++ module/DefaultModule/src/DefaultModule/Test/Controller/SignControllerTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use DefaultModule\Test\Controller\AbstractTestCase;
+use Zend\Http\Request;
+use Users\Fixture\UserCredentials;
 
 /**
  * SignControllerTest Tests for SignController
@@ -11,14 +13,27 @@ use DefaultModule\Test\Controller\AbstractTestCase;
 class SignControllerTest extends AbstractTestCase
 {
 
-   public function testInAction()
-   {
-       echo "true";
-   }
-   
-   public function testOutAction()
-   {
-       echo "again";
-   }
-    
+    /**
+     * Tests inAction with valid credentials submitted
+     * 
+     * @access public
+     * 
+     * @covers SignController::inAction()
+     */
+    public function testInAction()
+    {
+        $this->fixtureLoader->loadFixturesFromEntities(array("Users\Entity\User"));
+        $userCredentials = UserCredentials::$userCredentials;
+        $randomUsername = array_rand($userCredentials);
+        
+        // sign in with random user credentials
+        $this->dispatch("/sign/in", Request::METHOD_POST, array("username" => $randomUsername, "password" => $userCredentials[$randomUsername]["password"]));
+
+        // Assert response is a redirect
+        $this->assertRedirect();
+        
+        // Assert redirect to home page
+        $this->assertRedirectTo('/');
+    }
+
 }
