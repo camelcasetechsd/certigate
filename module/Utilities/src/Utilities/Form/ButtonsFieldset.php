d8e0f05dbf7ea6bccf75b79fa52ff80d58aa8066
Mohamed Labib <lebaz20@gmail.com>
2016-03-07 15:33:50 +0200

    #87 Add course event form - Edit form buttons fieldset to support normal button layout - Edit create button text in fieldset via form view helper

--- module/Utilities/src/Utilities/Form/ButtonsFieldset.php
+++ module/Utilities/src/Utilities/Form/ButtonsFieldset.php
@@ -15,44 +15,60 @@ use Zend\Form\Fieldset;
  */
 class ButtonsFieldset extends Fieldset
 {
-    
+
     /**
      * setup form
      * 
      * @access public
+     * 
+     * @param string $name ,default is null
+     * @param array $options ,default is empty array
      */
-    public function __construct()
+    public function __construct($name = null, $options = array())
     {
-        parent::__construct(/*$name =*/ "buttons");
-        
-        $this->add( array(
-            'name' => FormButtons::SAVE_BUTTON,
-            'type' => 'Zend\Form\Element\Submit',
-            'attributes' => array(
-                'class' => 'pull-left btn-inline btn btn-primary',
-                'value' => FormButtons::SAVE_BUTTON_TEXT,
-            )
-        ) );
-        
-        $this->add( array(
-            'name' => FormButtons::SAVE_AND_PUBLISH_BUTTON,
-            'type' => 'Zend\Form\Element\Submit',
-            'attributes' => array(
-                'class' => 'pull-left btn-inline btn btn-success',
-                'value' => FormButtons::SAVE_AND_PUBLISH_BUTTON_TEXT,
-            )
-        ) );
-        
-        $this->add( array(
-            'name' => FormButtons::UNPUBLISH_BUTTON,
-            'type' => 'Zend\Form\Element\Submit',
-            'attributes' => array(
-                'class' => 'pull-left btn-inline btn btn-warning',
-                'value' => FormButtons::UNPUBLISH_BUTTON_TEXT,
-            )
-        ) );
+        parent::__construct(/* $name = */ (!empty($name)) ? $name : "buttons", $options);
 
-        $this->add( array(
+        $createButtonOnly = $options["create_button_only"];
+        if ($createButtonOnly === true) {
+            $this->add(array(
+                'name' => FormButtons::CREATE_BUTTON,
+                'type' => 'Zend\Form\Element\Submit',
+                'attributes' => array(
+                    'class' => 'pull-left btn-inline btn btn-success',
+                    'value' => FormButtons::CREATE_BUTTON_TEXT,
+                )
+            ));
+        }
+        else {
+            $this->add(array(
+                'name' => FormButtons::SAVE_BUTTON,
+                'type' => 'Zend\Form\Element\Submit',
+                'attributes' => array(
+                    'class' => 'pull-left btn-inline btn btn-primary',
+                    'value' => FormButtons::SAVE_BUTTON_TEXT,
+                )
+            ));
+
+            $this->add(array(
+                'name' => FormButtons::SAVE_AND_PUBLISH_BUTTON,
+                'type' => 'Zend\Form\Element\Submit',
+                'attributes' => array(
+                    'class' => 'pull-left btn-inline btn btn-success',
+                    'value' => FormButtons::SAVE_AND_PUBLISH_BUTTON_TEXT,
+                )
+            ));
+
+            $this->add(array(
+                'name' => FormButtons::UNPUBLISH_BUTTON,
+                'type' => 'Zend\Form\Element\Submit',
+                'attributes' => array(
+                    'class' => 'pull-left btn-inline btn btn-warning',
+                    'value' => FormButtons::UNPUBLISH_BUTTON_TEXT,
+                )
+            ));
+        }
+
+        $this->add(array(
             'name' => FormButtons::RESET_BUTTON,
             'type' => 'Zend\Form\Element',
             'attributes' => array(
@@ -60,6 +76,7 @@ class ButtonsFieldset extends Fieldset
                 'value' => FormButtons::RESET_BUTTON_TEXT,
                 'type' => 'button',
             )
-        ) );
+        ));
     }
+
 }
