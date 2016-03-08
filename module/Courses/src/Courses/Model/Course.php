9b4a2b0dd3e6669992c309044c4d2cb4df7b556c
ahmedReda1 <se_ahmedreda@yahoo.com>
2016-03-08 14:29:44 +0200

    #89 removing duplicate code

--- module/Courses/src/Courses/Model/Course.php
+++ module/Courses/src/Courses/Model/Course.php
@@ -11,7 +11,6 @@ use Utilities\Service\Paginator\PaginatorAdapter;
 use Zend\Paginator\Paginator;
 use Doctrine\Common\Collections\Criteria;
 
-
 /**
  * Course Model
  * 
@@ -31,6 +30,7 @@ class Course
 {
 
     use \Utilities\Service\Paginator\PaginatorTrait;
+
     /**
      *
      * @var Utilities\Service\Query\Query 
@@ -60,12 +60,6 @@ class Course
      * @var Versioning\Model\Version
      */
     protected $version;
-
-    /**
-     *
-     * @var Zend\Paginator\Paginator 
-     */
-    protected $paginator = NULL;
 
     /**
      * Set needed properties
@@ -220,140 +214,6 @@ class Course
     }
 
     /**
-     * Set current page
-     * @param int $currentPage
-     */
-    public function setPage($currentPage)
-    {
-        $this->paginator->setCurrentPageNumber($currentPage);
-    }
-
-    /**
-     * Set number of pages
-     * @param int $numberPerPage
-     */
-    public function setNumberPerPage($numberPerPage)
-    {
-        $this->numberPerPage = $numberPerPage;
-    }
-
-    /**
-     * Set number of items per page
-     * @param int $itemsCountPerPage
-     */
-    public function setItemCountPerPage($itemsCountPerPage)
-    {
-        $this->paginator->setItemCountPerPage($itemsCountPerPage);
-    }
-
-    /**
-     * Get number of pages
-     * @return int number of pages
-     */
-    public function getNumberOfPages()
-    {
-        return (int) $this->paginator->count();
-    }
-
-    /**
-     * Get pages range
-     * 
-     * @access public
-     * 
-     * @param int $currentPageNumber ,default is null
-     * @return array pages range
-     */
-    public function getPagesRange($currentPageNumber = null)
-    {
-
-        $numberOfPages = $this->getNumberOfPages();
-        $pageNumbers = array();
-        //create an array of page numbers
-        if ($numberOfPages > 1) {
-            $pageNumbers = range(1, $numberOfPages);
-
-
-            foreach ($pageNumbers as $pageKey => &$pageNumber) {
-                $pageNumber = array(
-                    "pageNumber" => $pageKey + 1,
-                    "isCurrent" => false
-                );
-            }
-
-            if (empty($currentPageNumber)) {
-                $currentPageNumber = 1;
-            }
-            $pageNumbers[$currentPageNumber - 1]["isCurrent"] = true;
-        }
-        return $pageNumbers;
-    }
-
-    /**
-     * Get next page number
-     * 
-     * @access public
-     * 
-     * @param int $currentPageNumber ,default is null
-     * @return int next page number
-     */
-    public function getNextPageNumber($currentPageNumber = null)
-    {
-        $nextPageNumber = 0;
-        $numberOfPages = $this->getNumberOfPages();
-        if ($numberOfPages > 1) {
-            if (!empty($currentPageNumber)) {
-                if ($currentPageNumber != $numberOfPages) {
-                    $nextPageNumber += $currentPageNumber + 1;
-                }
-            }
-            else {
-                $nextPageNumber = 2;
-            }
-        }
-
-        return $nextPageNumber;
-    }
-
-    /**
-     * Get previous page number
-     * 
-     * @access public
-     * 
-     * @param int $currentPageNumber ,default is null
-     * @return int previous page number
-     */
-    public function getPreviousPageNumber($currentPageNumber = null)
-    {
-        $previousPageNumber = 0;
-        $numberOfPages = $this->getNumberOfPages();
-        if ($numberOfPages > 1 && !empty($currentPageNumber)) {
-            $previousPageNumber += $currentPageNumber - 1;
-        }
-
-        return $previousPageNumber;
-    }
-
-    /**
-     * Get number of items per page
-     * @return int number of items per page
-     */
-    public function getCurrentItems()
-    {
-        return $this->paginator;
-    }
-
-    /**
-     * Set criteria
-     * 
-     * @access public
-     * 
-     * @param Doctrine\Common\Collections\Criteria $criteria
-     */
-    public function setCriteria($criteria)
-    {
-        $this->paginator->getAdapter()->setCriteria($criteria);
-}
-    /**
      * Filter courses
      * 
      * @access public
@@ -369,7 +229,6 @@ class Course
         }
         $this->setCriteria($criteria);
         $this->setItemCountPerPage(1);
-
     }
 
 }
