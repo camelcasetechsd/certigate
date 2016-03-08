ba26c12484e6cf042cf5d73589f3a7691c84dc5e
ahmedReda1 <se_ahmedreda@yahoo.com>
2016-03-08 12:45:30 +0200

    #91 send to friend mail fixed

---
+++ module/Utilities/src/Utilities/Service/MessageTypes.php
@@ -0,0 +1,33 @@
+<?php
+namespace Utilities\Service;
+/**
+ * MessageTypes
+ * 
+ * Hold Message Types related constants
+ * 
+ * 
+ * @package utilities
+ * @subpackage service
+ */
+class MessageTypes {
+    
+    /**
+     * warning message type
+     */
+    const WARNING = "warning";
+    
+    /**
+     * danger message type
+     */
+    const DANGER = "danger";
+    
+    /**
+     * success message type
+     */
+    const SUCCESS = "success";
+    
+    /**
+     * info message type
+     */
+    const INFO = "info";
+}
