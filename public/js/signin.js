82d4177f824084b2ef35edf2312b2efb0e6a2327
ahmed <ahmed.elbougha@camelcasetech.com>
2016-03-20 18:07:58 +0200

    Quick Fix

--- public/js/signin.js
+++ public/js/signin.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     if($("form[name=signin_header_form]").length){
         var form = $("form[name=signin_header_form]");
         var action = form.attr("action");
-        var queryString = window.location.href.slice(window.location.href.indexOf('?'));
+        var queryString = (window.location.href.indexOf('?') >= 0) ? window.location.href.slice(window.location.href.indexOf('?')) : '';
         form.attr("action", action + queryString);
     }
 });
