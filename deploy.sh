ff756d4c4d89670833534e1b37bb37f16d3f4e2d
Adham Saad <adham.sa3d@gmail.com>
2016-03-27 14:55:54 +0200

    add APPLICATION_ENV to all commands

--- deploy.sh
+++ deploy.sh
@@ -10,17 +10,17 @@ bin/doctrine orm:schema-tool:drop --force;
 bin/doctrine orm:schema-tool:update --force;
 
 # seeding data
+app_env=${APPLICATION_ENV:-'vagrant'}
 
 # seeding estore first 
-php public/estore/updateDB.php
+APPLICATION_ENV=$app_env php public/estore/updateDB.php
 
 # running certigate seeds 
-app_env=${APPLICATION_ENV:-'vagrant'}
 # send app_env as environment variable and command variable 
 APPLICATION_ENV=$app_env ./vendor/bin/phinx seed:run -e $app_env 
 
 # seeding q2a
-php public/q2a/updateDB.php
+APPLICATION_ENV=$app_env php public/q2a/updateDB.php
 
 ## prepare public resources
 cd public
