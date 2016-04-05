da8e69eb6195148f32e85a201f12924474646f63
ahmedReda1 <se_ahmedreda@yahoo.com>
2016-04-05 13:53:46 +0200

    #104 fix datepicker add gregorian-ar class

--- public/js/datepicker.js
+++ public/js/datepicker.js
@@ -13,7 +13,7 @@ $.calendars.calendars.islamic.prototype.regional['ar'] = {
         'رجب', 'شعبان', 'رمضان', 'شوّال', 'ذو القعدة', 'ذو الحجة'],
     monthNamesShort: ['محرّم', 'صفر', 'ربيع الأول', 'ربيع الآخر أو ربيع الثاني', 'جمادى الاول', 'جمادى الآخر أو جمادى الثاني',
         'رجب', 'شعبان', 'رمضان', 'شوّال', 'ذو القعدة', 'ذو الحجة'],
-    dayNames: ['يوم الأحد', 'يوم الإثنين', 'يوم الثلاثاء', 'يوم الأربعاء', 'يوم الخميس', 'يوم الجمعة', 'يوم السبت'],
+    dayNames: ['الأحد', 'الإثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت'],
     dayNamesShort: ['يوم الأحد', 'يوم الإثنين', 'يوم الثلاثاء', 'يوم الأربعاء', 'يوم الخميس', 'يوم الجمعة', 'يوم السبت'],
     dayNamesMin: ['يوم الأحد', 'يوم الإثنين', 'يوم الثلاثاء', 'يوم الأربعاء', 'يوم الخميس', 'يوم الجمعة', 'يوم السبت'],
     digits: $.calendars.substituteDigits(['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩']),
@@ -21,7 +21,7 @@ $.calendars.calendars.islamic.prototype.regional['ar'] = {
     firstDay: 6,
     isRTL: true
 };
-
+// rtl date pickers classes
 $('.hijriDate-ar').calendarsPicker($.extend({
     calendar: $.calendars.instance('islamic', 'ar'),
     onClose: function (dateText, datePickerInstance) {
@@ -32,17 +32,35 @@ $('.hijriDate-ar').calendarsPicker($.extend({
             // creating gregorian date out of julain date
             var gregorianDate = $.calendars.instance('gregorian').fromJD(julianDate);
             console.log('hijri is ' + dateText.toLocaleString() + ' Leads to julian: ' + julianDate + ' which is in gregorian ' + gregorianDate);
-            $(this).parent().next('div').find('.gregorianDate').val(gregorianDate.formatDate('dd/mm/yyyy').toLocaleString().replace(/-/g, '/'));
+            $(this).parent().next('div').find('.gregorianDate-ar').val(gregorianDate.formatDate('dd/mm/yyyy').toLocaleString().replace(/-/g, '/'));
         } else {
-            $(this).parent().next('div').find('.gregorianDate').val("");
+            $(this).parent().next('div').find('.gregorianDate-ar').val("");
         }
     }
 }));
 
+$('.gregorianDate-ar').calendarsPicker($.extend({
+    calendar: $.calendars.instance('gregorian'),
+    onClose: function (dateText, datePickerInstance) {
+        $(this).val(dateText[0].formatDate('dd/mm/yyyy'));
+        // getting julian date out of hijri date
+        if (dateText.length > 0) {
+            var julianDate = dateText[0].toJD();
+            // creating gregorian date out of julain date
+            var islamicDate = $.calendars.instance('islamic').fromJD(julianDate);
+            console.log('gregorian is ' + dateText.toLocaleString() + ' Leads to julian: ' + julianDate + ' which is in islamic ' + islamicDate);
+            $(this).parent().prev('div').find('.hijriDate-ar').val(islamicDate.formatDate('dd/mm/yyyy').toLocaleString().replace(/-/g, '/'));
+        } else {
+            $(this).parent().prev('div').find('.hijriDate-ar').val("");
+
+        }
+
+    }
+}));
+
+// ltr date pickers classes
 $('.hijriDate').calendarsPicker($.extend({
     calendar: $.calendars.instance('islamic'),
-    onselect: function (dateText) {
-    },
     onClose: function (dateText, datePickerInstance) {
         $(this).val(dateText[0].formatDate('dd/mm/yyyy'));
         // getting julian date out of hijri date
@@ -55,9 +73,7 @@ $('.hijriDate').calendarsPicker($.extend({
             $(this).parent().next('div').find('.gregorianDate').val(gregorianDate.formatDate('dd/mm/yyyy').toLocaleString().replace(/-/g, '/'));
         } else {
             $(this).parent().next('div').find('.gregorianDate').val("");
-
         }
-
     }
 }));
 
