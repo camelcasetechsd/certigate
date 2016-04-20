317cca982f7428bb265c8f5c8de321b260d69f86
Mohamed Labib <lebaz20@gmail.com>
2016-04-20 13:50:48 +0200

    #98 Add new plugin for autologin in codoforum

---
+++ public/forum/sites/default/plugins/autologin/autologin.info.php
@@ -0,0 +1,9 @@
+<?php
+
+  $info['name'] = 'Auto login';
+  $info['description'] = 'A Plugin to autologin any user in main app';
+  $info['version'] = '1.x';
+  $info['author'] = "certigate";
+  $info['author_url'] = 'http://certigate.com';
+  $info['license'] = 'MIT License';
+  $info['core'] = '3.x'; //works with which version of the forum?
