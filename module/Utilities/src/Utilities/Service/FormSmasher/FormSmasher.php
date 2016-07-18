a4668831063daae438303594e38e4b8125a3b5f7
ahmedReda1 <se_ahmedreda@yahoo.com>
2016-07-18 18:06:33 +0200

    #194 fix adding id to elements in form smasher

--- module/Utilities/src/Utilities/Service/FormSmasher/FormSmasher.php
+++ module/Utilities/src/Utilities/Service/FormSmasher/FormSmasher.php
@@ -47,6 +47,7 @@ class FormSmasher
          * Form open && close tags
          */
         $form->setAttribute('enctype', 'multipart/form-data');
+        $formName = $form->getAttributes()['name'];
         $elementsContainer['openTag'] = $this->viewHelperManager->get('form')->openTag($form);
         $elementsContainer['closeTag'] = $this->viewHelperManager->get('form')->closeTag();
 
@@ -56,51 +57,51 @@ class FormSmasher
             $elementsContainer[$name . 'Label'] = $element->getLabel();
 
             if ($element->getAttribute('type') === 'captcha' || $element->getAttribute('name') === 'captcha') {
-                $elementsContainer[$name] = $formCaptcha($form->get($name));
+                $elementsContainer[$name] = $formCaptcha($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)));
             }
             else if ($element->getAttribute('type') === 'text') {
-                $elementsContainer[$name] = $formInput($form->get($name));
+                $elementsContainer[$name] = $formInput($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)), $element->setAttributes(array('id' => $formName . '_' . $name)));
             }
             else if ($element->getAttribute('type') === 'textarea') {
-                $elementsContainer[$name] = $formTextarea($form->get($name));
+                $elementsContainer[$name] = $formTextarea($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)));
             }
             else if ($element->getAttribute('type') === 'password') {
-                $elementsContainer[$name] = $formPassword($form->get($name));
+                $elementsContainer[$name] = $formPassword($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)));
             }
             else if ($element->getAttribute('type') === 'select') {
-                $elementsContainer[$name] = $formSelect($form->get($name), $element->getValue(), $element->getAttributes(), $element->getOptions());
+                $elementsContainer[$name] = $formSelect($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)), $element->getValue(), $element->getAttributes(), $element->getOptions());
             }
             else if ($element->getAttribute('type') === 'number') {
-                $elementsContainer[$name] = $formNumber($form->get($name));
+                $elementsContainer[$name] = $formNumber($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)));
             }
             else if ($element->getAttribute('type') === 'time') {
-                $elementsContainer[$name] = $formTime($form->get($name));
+                $elementsContainer[$name] = $formTime($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)));
             }
             else if ($element->getAttribute('type') === 'file') {
-                $elementsContainer[$name] = $formFile($form->get($name));
+                $elementsContainer[$name] = $formFile($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)));
             }
             else if ($element->getAttribute('type') === 'hidden') {
-                $elementsContainer[$name] = $formHidden($form->get($name), $element->getValue(), $element->getAttributes());
+                $elementsContainer[$name] = $formHidden($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)), $element->getValue(), $element->getAttributes());
             }
             else if ($element->getAttribute('type') === 'image') {
-                $elementsContainer[$name] = $formImage($form->get($name), $element->getValue(), $element->getAttributes());
+                $elementsContainer[$name] = $formImage($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)), $element->getValue(), $element->getAttributes());
             }
             else if ($element->getAttribute('type') === 'email') {
-                $elementsContainer[$name] = $formEmail($form->get($name));
+                $elementsContainer[$name] = $formEmail($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)));
             }
             else if ($element->getAttribute('type') === 'checkbox') {
-                $elementsContainer[$name] = $formCheckbox($form->get($name));
+                $elementsContainer[$name] = $formCheckbox($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)));
             }
             else if ($element->getAttribute('type') === 'multi_checkbox') {
-                $elementsContainer[$name] = $formMultiCheckbox($form->get($name), 'append');
+                $elementsContainer[$name] = $formMultiCheckbox($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)), 'append');
             }
             else if ($element->getAttribute('type') === 'radio') {
-                $elementsContainer[$name] = $formRadio($form->get($name));
+                $elementsContainer[$name] = $formRadio($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)));
             }
             else if ($element->getAttribute('type') === 'button') {
                 $elementsContainer[$name] = $formButton($form->get($name), $element->getValue());
+//                $elementsContainer[$name] = $formButton($form->get($name), $element->setAttributes(array('id' => $formName . '_' . $name)), $element->getValue());
             }
-            $element->setAttribute('id', $name);
             $elementsContainer[$name . 'Error'] = $formElementErrors($element);
             $elementsContainer[$name . 'Name'] = $name;
         }
