8e65f4739597881b13d6f13438e9287340b38bfe
Adham Saad <adham.sa3d@gmail.com>
2016-07-18 15:33:31 +0200

    changing chat server port

--- module/Chat/src/Chat/Controller/ChatController.php
+++ module/Chat/src/Chat/Controller/ChatController.php
@@ -24,7 +24,7 @@ class ChatController extends ActionController
     public function runServerAction()
     {
         try{
-            $server = IoServer::factory(new HttpServer(new WsServer($this->getServiceLocator()->get('Chat\Service\ChatServer'))), 10100);
+            $server = IoServer::factory(new HttpServer(new WsServer($this->getServiceLocator()->get('Chat\Service\ChatServer'))), 2083);
             $server->run();
         } catch (\Exception $ex) {
             // it is already running 
