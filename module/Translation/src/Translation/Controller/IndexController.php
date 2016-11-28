f804cf3c935f9deb005d15c72f84fb1a29ef6a35
Adham Saad <adham.sa3d@gmail.com>
2016-11-28 12:58:59 +0200

    #220 - flush menu cache when switching languages

--- module/Translation/src/Translation/Controller/IndexController.php
+++ module/Translation/src/Translation/Controller/IndexController.php
@@ -16,6 +16,10 @@ class IndexController extends \Utilities\Controller\ActionController
     {
         $locale = $this->params('locale');
         
+        /* @var $cmsCacheHandler \CMS\Service\Cache\CacheHandler */
+        $cmsCacheHandler = $this->getServiceLocator()->get('cmsCacheHandler');
+        $cmsCacheHandler->flushCMSCache(\CMS\Service\Cache\CacheHandler::MENUS_KEY);
+        
         /* @var $applicationLocale \Translation\Service\Locale\Locale */
         $applicationLocale = $this->getServiceLocator()->get('applicationLocale');
         $applicationLocale->setCurrentLocale($locale);
